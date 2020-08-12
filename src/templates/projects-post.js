cc78dccd15540e511648c6439f52320399785682
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-08-12 17:28:13 -0400

    added github and Webite buttoms

--- src/templates/projects-post.js
+++ src/templates/projects-post.js
@@ -1,24 +1,43 @@
-import React from "react"
+import React, { useContext } from "react"
+import ThemeContext from "../utils/theme"
 import { graphql } from "gatsby"
 import PostTemplate from "./post-template"
 import Badge from "react-bootstrap/Badge"
+import { Button } from "react-bootstrap"
 
-const SubTitle = ({ tags }) => (
-  <div className="mb-5">
-    {tags.map(tag => (
-      <Badge key={tag} pill variant="dark" className="px-3 mr-1">
-        <h5 className="text-white my-0">{tag}</h5>
-      </Badge>
-    ))}
-  </div>
-)
+
+const SubTitle = ({ tags, github, website }) => {
+  const { dark } = useContext(ThemeContext)
+  return (
+    <div className="mb-5">
+      {tags.map(tag => (
+        <Badge key={tag} pill variant="dark" className="px-3 m-1">
+          <h5 className="text-white my-0">{tag}</h5>
+        </Badge>
+      ))}
+      <div className="mt-4">
+        <Button
+          className="mr-4"
+          href={github}
+          target="_blank"
+          variant={`${dark ? "outline-light" : "outline-primary" }`}
+        >View on GitHub</Button>
+        <Button
+          href={website}
+          target="_blank"
+          variant={`${dark ? "outline-light" : "outline-primary" }`}
+        >Visit the Website</Button>
+      </div>
+    </div>
+  )
+}
 
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
     <PostTemplate
       title={post.frontmatter.title}
-      subTitle={<SubTitle tags={post.frontmatter.tags} />}
+      subTitle={<SubTitle tags={post.frontmatter.tags} github={post.frontmatter.githubLink} website={post.frontmatter.websiteLink} />}
       excerpt={post.excerpt}
       html={post.html}
     />
@@ -32,6 +51,8 @@ export const query = graphql`
       frontmatter {
         title
         tags
+        githubLink
+        websiteLink
       }
       excerpt
     }
